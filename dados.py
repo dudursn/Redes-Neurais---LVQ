f4b92683a1d458ec9c05e2131cbd718685d7c2fe
Eduardo Roger <roger.rsn@outlook.com>
2018-11-17 21:37:36 -0200

    implementacao final

---
+++ dados.py
@@ -0,0 +1,29 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+def normalizar_dados(data):
+	for i in range(4):
+		data[:,i] = (data[:,i] - min(data[:,i]))/(max(data[:,i]) - min(data[:,i]))
+	return data
+
+def pegar_dados():
+	my_data = np.genfromtxt('iris.csv', delimiter=';')
+	my_data = normalizar_dados(my_data)
+	baseEntrada, baseSaida = my_data[:,:4], my_data[:,4]
+	X, X_teste, Y, Y_teste = train_test_split(baseEntrada, baseSaida, test_size=0.3, random_state=0)
+
+	baseTreino = []
+	for i in range(len(Y)):
+		instancia = {}
+		instancia['classe'] = int(Y[i])
+		instancia['vetor'] = X[i]
+		baseTreino.append(instancia) 
+	baseTeste = []
+	for i in range(len(Y_teste)):
+		instancia = {}
+		instancia['classe'] = int(Y_teste[i])
+		instancia['vetor'] = X_teste[i]
+		baseTeste.append(instancia)
+
+	return baseTreino, baseTeste
+
---
+++ lvq_iris.py
@@ -0,0 +1,100 @@
+import numpy as np
+from random import random,choice
+from math import sqrt
+import dados
+
+#Gera os valores de uma entrada(gera os pesos do vetor prototipo)
+def vetor_aleatorio(dominio):
+	valores = []
+	numAtributos = len(dominio)
+	minimo = dominio[0][0]
+	maximo = dominio[0][1]
+	for i in range(numAtributos):
+		valor = minimo + ((maximo - minimo) * random())
+		valores.append(valor)
+	return valores
+
+  
+def imprimePrototipos(prototipos):
+	for prototipo in prototipos:
+		print(prototipo)
+	
+
+def dist_euclidiana(vetor1, vetor2):
+	res = (np.subtract(vetor1, vetor2))**2
+	soma = np.sum(res)
+	return sqrt(soma)
+
+#Cria os codebooks vectors ou protótipos
+def inicializa_prototipos(classes, numPrototipos):
+	prototipos = []
+	#Os dados foram normalizados entre 0 e 1
+	dominioDosAtributos = [[0,1] for x in range(4)]
+	for i in range(numPrototipos):
+		prototipo = {}
+		classeSelecionada = choice(classes)
+		prototipo['classe'] = classeSelecionada
+		prototipo['vetor'] = vetor_aleatorio(dominioDosAtributos)
+		prototipos.append(prototipo)
+	return prototipos
+
+#Faz o ranqueamento do melhor prototipo ou codebook
+def pegar_unidade_de_melhor_correspondencia(prototipos, entrada):
+	ranqueamento = []
+	for prototipo in prototipos:
+		distancia = dist_euclidiana(entrada['vetor'], prototipo['vetor'])
+		ranqueamento.append((distancia, prototipo))
+	melhorPrototipo = sorted(ranqueamento)[0][1]
+	return melhorPrototipo
+
+#Atualiza o melhor prototipo
+def atualiza_prototipo(melhorPrototipo, entrada, novaTaxaDeApendizagem):
+	numAtributos = len(melhorPrototipo['vetor'])
+	for i in range(numAtributos):
+		erro = entrada['vetor'][i] - melhorPrototipo['vetor'][i]
+		if melhorPrototipo['classe'] == entrada['classe']:
+			melhorPrototipo['vetor'][i] += novaTaxaDeApendizagem * erro
+		else:
+			melhorPrototipo['vetor'][i] -= novaTaxaDeApendizagem * erro
+
+def treino(classes, baseTreino, numPrototipos, taxaDeAprendizagem):
+	prototipos = inicializa_prototipos(classes, numPrototipos)
+	novaTaxaDeApendizagem = taxaDeAprendizagem
+
+	iteracoes = len(baseTreino)
+	for i in range(iteracoes):
+		entradaTreino = baseTreino[i]
+		melhorPrototipo = pegar_unidade_de_melhor_correspondencia(prototipos, entradaTreino)
+		novaTaxaDeApendizagem = taxaDeAprendizagem*(1.0 - (i/iteracoes))
+		atualiza_prototipo(melhorPrototipo, entradaTreino, novaTaxaDeApendizagem)
+		print("Iteracao", i,":",novaTaxaDeApendizagem)
+		if(novaTaxaDeApendizagem<=0.0):
+			break
+		#print(" Classe do Melhor Prototipo:", melhorPrototipo['classe'], ", Classe da Entrada:",entradaTreino['classe'])
+	return prototipos
+
+def teste(prototipos, baseTeste):
+	corretos = 0 
+	numTestes = len(baseTeste)
+
+	for i in range(numTestes):
+		entradaTeste = baseTeste[i]
+		melhorPrototipo = pegar_unidade_de_melhor_correspondencia(prototipos, entradaTeste)
+		if melhorPrototipo['classe']== entradaTeste['classe']:
+			corretos+=1
+	print("Feito. Corretos:",corretos,"/",numTestes)
+	acertos = (corretos/numTestes) * 100.0
+	print("Acurácia: %.2f" % (acertos))
+
+if __name__ == '__main__':
+	# Dados
+	baseTreino, baseTeste = dados.pegar_dados() 
+	classes = [0,1,2]
+	# Parametros do algoritmo
+	taxaDeAprendizagem = 0.3
+	numPrototipos = 25
+	#Executar o algoritmo
+	prototipos = treino(classes ,baseTreino, numPrototipos, taxaDeAprendizagem)
+	teste(prototipos, baseTeste)
+	imprimePrototipos(prototipos)
+	
